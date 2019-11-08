e0d260b710244c55d6b3ce34b03116d997f43e12
liulei <satbirdd@126.com>
2019-11-08 14:12:31 +0800

    use full nano seconds

--- utils.go
+++ utils.go
@@ -3,7 +3,7 @@ package utils
 import "time"
 
 func ShanghaiDatetime(dateStr string) (*time.Time, error) {
-	timeLayout := "2006-01-02 15:04:05.999"
+	timeLayout := "2006-01-02 15:04:05.999999"
 	loc, _ := time.LoadLocation("Asia/Shanghai")
 	t, err := time.ParseInLocation(timeLayout, dateStr, loc)
 	if err != nil {
